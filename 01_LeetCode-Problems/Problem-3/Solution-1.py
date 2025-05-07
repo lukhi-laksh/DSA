55b1b506421be381943b14adc22f8246ae9a673e
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-07 16:52:18 +0530

    Problem solved

---
+++ 01_LeetCode-Problems/Problem-3/Solution-1.py
@@ -0,0 +1,33 @@
+"""
+Given a string s, find the length of the longest substring without duplicate characters.
+
+"""
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+    
+        length = len(s)
+        max_length = 0
+        longest_substr = ""
+        temp = list()   
+        for i in range(length):
+            for j in range(i+1, length+1):
+                subString = s[i:j]
+                if len(subString) == len(set(subString)):
+                    if len(subString) > max_length:
+                        max_length = len(subString)
+                        longest_substr = subString
+
+        print("Longest substring with unique characters:", longest_substr)
+        print("Size of the longest substring:", max_length)
+        return max_length
+            
+
+sol = Solution()
+s = "pwwkew"
+print(sol.lengthOfLongestSubstring(s))
+
+"""
+Time Complexity:  O(n³)
+Space Complexity: O(k)
+
+"""
