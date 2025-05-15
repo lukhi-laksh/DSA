9256ef5f333bb2f9618656d096cb66a18570811b
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-15 18:00:57 +0530

    Pattern

---
+++ 04_Patterns/Pattern_24.py
@@ -0,0 +1,7 @@
+for i in range(1, 8):
+    for j in range(1, 8):
+        if (i == 1 or i == 7 or j == 1 or j == 7) or ((i >= 3 and i <= 5) and (j >= 3 and j <= 5) and (i == 3 or i == 5 or j == 3 or j == 5)):
+            print("*", end="  ")  
+        else:
+            print(" ", end="  ")     
+    print("")
