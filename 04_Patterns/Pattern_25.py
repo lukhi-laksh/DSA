8f10f15f6c70f81fe8b4d541618347d953a8f615
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-16 12:51:20 +0530

    Pattern

---
+++ 04_Patterns/Pattern_25.py
@@ -0,0 +1,21 @@
+m = 0
+
+for i in range(1, 22):  # i from 1 to 21
+    if i <= 11:
+        m += 1
+    else:
+        m -= 1
+
+    k = m - 1
+
+    for j in range(1, 22):  # j from 1 to 21
+        if j >= 12 - m and j <= 10 + m:
+            if j < 11:
+                print(f" {k % 10}", end="")
+                k += 1
+            else:
+                print(f" {k % 10}", end="")
+                k -= 1
+        else:
+            print("  ", end="")  # Two spaces for alignment
+    print()
