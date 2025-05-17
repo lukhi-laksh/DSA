78d255c37e28bf2e4dc04eb8f0abb7adb08a639a
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-17 20:10:58 +0530

    Pattern

---
+++ 04_Patterns/Pattern_31.py
@@ -0,0 +1,11 @@
+k = 0
+for i in range(1, 6):
+    p = k 
+    for j in range(1, 6):
+        if j >= 6-i: 
+            print(p, end="  ")
+            p = p - 1
+        else:
+            print(" ", end="  ")
+    print("")
+    k += i + 1
