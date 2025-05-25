a40b18b4a3cb51cbf36847db64c03059001fc02c
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-25 22:47:49 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Contest/Weekly Contest/451/Problem-1.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def smallestIndex(self, nums):
+        for i in range(len(nums)):
+            if nums[i] == i:
+                return i
+        return -1
+
+sol = Solution()
+nums = []
+print(sol.smallestIndex(nums))
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+"""
