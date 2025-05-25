36186ae98517659a8c0e86ea8537594d45d55245
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-25 22:44:13 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-2131/Solution-1.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def longestPalindrome(self, words):
+
+        from collections import Counter
+
+        count = Counter(words)
+        length = 0
+        has_center = False
+
+        for word in list(count.keys()):
+            rev = word[::-1]
+            if word != rev and count[word] > 0 and count[rev] > 0:
+                pair = min(count[word], count[rev])
+                length += pair * 4
+                count[word] -= pair
+                count[rev] -= pair
+
+        for word in count:
+            if word[0] == word[1] and count[word] > 0:
+                pair = count[word] // 2
+                length += pair * 4
+                if count[word] % 2 == 1:
+                    has_center = True
+
+        if has_center:
+            length += 2
+
+        return length
