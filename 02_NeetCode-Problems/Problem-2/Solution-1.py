3c16441d7c8ac7873fb974581078b33d8748d0b0
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-05-26 16:24:05 +0530

    Problem Solved

--- 02_NeetCode-Problems/Problem-2/Solution-1.py
+++ 02_NeetCode-Problems/Problem-2/Solution-1.py
@@ -1,3 +1,9 @@
+"""
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+"""
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
@@ -6,3 +12,9 @@ str1 = "laksh"
 str2 = "hskal"
 solutions = Solution()
 print(solutions.isAnagram(str1, str2)) 
+
+"""
+Time Complexity: O(n log n)
+Space Complexity: O(1)
+
+"""
--- 02_NeetCode-Problems/Problem-2/Solution-3.py
+++ 02_NeetCode-Problems/Problem-2/Solution-3.py
@@ -1,3 +1,9 @@
+"""
+Given two strings s and t, return true if the two strings are anagrams of each other, otherwise return false.
+
+An anagram is a string that contains the exact same characters as another string, but the order of the characters can be different.
+
+"""
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -20,3 +26,9 @@ sol = Solution()
 s = "llaksh"
 t = "aklsha"
 print(sol.isAnagram(s, t))
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+"""
