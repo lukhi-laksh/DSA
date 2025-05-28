449cb832913b4111c33b9526ab94ff79949a4aa9
lukhi-laksh <luxlukhi04@gmail.com>
2025-05-28 10:35:37 +0530

    Problem Solved

---
+++ 02_NeetCode-Problems/Problem-3/Solution-1.py
@@ -0,0 +1,10 @@
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
+
+sol = Solution()
+nums = [2, 7, 11, 15]   
+target = 9
+print(sol.twoSum(nums, target)) 
