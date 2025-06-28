24389036ccee26792ac0fef50bea56c8dcb0ff3d
lukhi-laksh <luxlukhi04@gmail.com>
2025-06-28 23:12:55 +0530

    Problem Solved

--- 03_Other-Problems/Problem-14.py
+++ 03_Other-Problems/Problem-14.py
@@ -2,6 +2,11 @@ s = "ababc"
 count = list()
 for i in range(len(s)):
     arr = list()
-
+    for j in range(i, len(s)):
+        if j in arr:
+            break
+        arr.append(j)
+        count.append(len(arr))
+print(max(count))
 
     
