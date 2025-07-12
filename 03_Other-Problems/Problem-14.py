d4cf4908f98dc522be75ce74c53c332296493c7d
laksh_lukhi <140299225+lukhi-laksh@users.noreply.github.com>
2025-07-12 23:33:17 +0530

    Problem Solved

--- 03_Other-Problems/Problem-14.py
+++ 03_Other-Problems/Problem-14.py
@@ -2,8 +2,11 @@
 s = "ababc"
 # Create empty list
 count = list()
+
+# Outer loop
 for i in range(len(s)):
     arr = list()
+    # Inner loop
     for j in range(i, len(s)):
         if j in arr:
             break
