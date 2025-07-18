34d8a8d6b6605bfd628411eb4b585b35c64d8871
lukhi-laksh <luxlukhi04@gmail.com>
2025-07-18 17:12:22 +0530

    horizontal complete

---
+++ 03_Other-Problems/Problem-15.py
@@ -0,0 +1,33 @@
+from typing import List
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def row(board):
+            for parent in board:
+                final = set()
+                for child in parent:
+                    if child == ".":
+                        continue
+                    if child in final:
+                        print("congraulation error accurs")
+                        return False
+                    final.add(child)
+            return True
+        
+        
+
+        
+
+sol = Solution()
+board = [
+    ["1", "2", ".", ".", "3", ".", ".", ".", "."],
+    ["4", ".", ".", "5", ".", ".", ".", ".", "."],
+    [".", "5", "8", ".", ".", ".", ".", ".", "3"],
+    ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+    [".", ".", ".", "8", ".", "3", ".", ".", "5"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", ".", ".", ".", ".", ".", "2", ".", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "8"],
+    [".", ".", ".", ".", "8", ".", ".", ".", "9"],
+]
+print(sol.isValidSudoku(board))
