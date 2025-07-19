03bf2d8aab369dd99b171180a90195bee63d2449
lukhi-laksh <luxlukhi04@gmail.com>
2025-07-19 23:08:38 +0530

    Problem Solved

--- 03_Other-Problems/Problem-16.py
+++ 03_Other-Problems/Problem-16.py
@@ -1,9 +1,11 @@
 class Solution(object):
     def removeSubfolders(self, folder):
 
-        folder.sort()  # sort lexicographically
+        folder.sort()
         res = []
+        # For each
         for dir in folder:
+            # Checking Condition
             if not res or not dir.startswith(res[-1] + '/'):
                 res.append(dir)
         return res
