d912b54f7124dadb0e885d2e836ad8fc5ceca6ae
lukhi-laksh <luxlukhi04@gmail.com>
2025-08-16 20:18:01 +0530

    problem solved

---
+++ 01_LeetCode-Problems/Problem-9/Solution-2.py
@@ -0,0 +1,10 @@
+"""
+Given an integer x, return true if x is a palindrome, and false otherwise.
+"""
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        y = list(str(x))
+            
+        z = y[::-1]
