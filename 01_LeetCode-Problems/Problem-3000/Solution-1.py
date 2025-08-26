f5eae87a25c8a23ddad70d91ff5dfd74cf06dde9
lukhi-laksh <luxlukhi04@gmail.com>
2025-08-26 16:47:25 +0530

    Problem Solved

--- 01_LeetCode-Problems/Problem-3000/Solution-1.py
+++ 01_LeetCode-Problems/Problem-3000/Solution-1.py
@@ -1,3 +1,6 @@
+"""
+Maximum Area of Longest Diagnal Rectangle
+"""
 class Solution(object):
     def areaOfMaxDiagonal(self, dimensions):
         dimensions = sorted(dimensions)
@@ -7,4 +10,16 @@ class Solution(object):
         c = max(d)
         a, b = d[c]
         return a*b
-        
+    
+
+sol = Solution()
+rectangles = [(3, 4), (5, 12), (8, 15), (7, 24)]
+
+print(sol.areaOfMaxDiagonal(rectangles))
+    
+"""
+
+Time Complexity:  O(n log n)
+Space Complexity: O(n)
+
+"""
