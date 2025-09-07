1b35aa821e4cd7f8ae72df80a28f65cbf426f75c
lukhi-laksh <luxlukhi04@gmail.com>
2025-09-07 21:01:28 +0530

    Problem Solved

---
+++ 03_Other-Problems/Problem-25.py
@@ -0,0 +1,23 @@
+"""
+3 Sum with target = 0
+"""
+class Solution(object):
+    def threeSum(self, nums):
+        ans = set()
+        n = len(nums)
+        nums.sort()
+        for i in range(n - 2):
+            left, right = i + 1, n - 1
+            while left < right:
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    temp = (nums[i], nums[left], nums[right])
+                    ans.add(temp)
+                    left += 1
+                    right -= 1
+                elif total < 0:
+                    left += 1
+                else:
+                    right -= 1
+       
+        return [list(t) for t in ans]
