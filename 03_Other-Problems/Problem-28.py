022015aedc8bc09d49f105322ee6accd1b4a0c47
lukhi-laksh <luxlukhi04@gmail.com>
2025-09-08 23:08:26 +0530

    Problem Solved

---
+++ 03_Other-Problems/Problem-28.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canThreePartsEqualSum(self, arr):
+        total = sum(arr)
+        if total % 3 != 0:
+            return False
+        target = total // 3
+        count = 0
+        curr_sum = 0
+        for i in range(len(arr)): 
+            curr_sum += arr[i]  
+            if curr_sum == target:
+                count += 1
+                curr_sum = 0
+            if count == 2 and i < len(arr) - 1:
+                return True
+        return False
+
+sol = Solution()
+print(sol.canThreePartsEqualSum([0,2,1,-6,6,-7,9,1,2,0,1]))
