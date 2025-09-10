b90da85327fc64451a004c9ca9c8cb055a8ed5c5
lukhi-laksh <luxlukhi04@gmail.com>
2025-09-10 18:09:11 +0530

    Problem solved

---
+++ 03_Other-Problems/Problem-35.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def makeTheIntegerZero(self, num1, num2):
+        for ops in range(1, 61):
+            target = num1 - ops * num2
+            if target <= 0:
+                continue
+            if ops <= target and target <= ops * (1 << 60):
+                if bin(target).count('1') <= ops <= target:
+                    return ops
+        
+        return -1
