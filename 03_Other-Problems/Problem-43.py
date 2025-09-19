9220cc3491d831dfadd7a51bdb2ca41c64cddb9a
lukhi-laksh <luxlukhi04@gmail.com>
2025-09-19 17:20:33 +0530

    Problem Solved

---
+++ 03_Other-Problems/Problem-43.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def countSubarrays(self, nums, k):
+        left = right = 0
+        windowSum = 0
+        valid = 0
+        while right < len(nums):
+            windowSum += nums[right]
+            right += 1
+            
+            while left < right and windowSum * (right - left) >= k:
+                windowSum -= nums[left] 
+                left += 1
+            
+            valid += right - left
+        
+        return valid
+
+sol = Solution()
+print(sol.countSubarrays([1, 3, 5, 3, 5, 3], 3))
