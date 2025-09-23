2ca8dcb05ccf9d5daedde7b2b24c2a4ae5411ce3
lukhi-laksh <luxlukhi04@gmail.com>
2025-09-23 21:48:57 +0530

    Problem Solved

---
+++ 03_Other-Problems/Problem-50.py
@@ -0,0 +1,31 @@
+from itertools import product
+
+
+class Solution(object):
+    def letterCombinations(self, digits):
+        digit_map = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        # step 1: map digits to characters
+        chars = [digit_map[digit] for digit in digits]
+        # step 2: obtain all possible combinations
+        combs = product(*chars)  # or product(chrs for chrs in chars)
+        result = []
+        for comb in combs:
+            if not comb:
+                continue
+            # step 3: concatenate characters
+            string = ''.join(comb)
+            # step 4: add to result list
+            result.append(string)
+        return result
+    
+sol = Solution()
+print(sol.letterCombinations('3343'))
