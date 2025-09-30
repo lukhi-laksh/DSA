b19ab3638b087c5b6afcd2d5a7909d395ee66f3c
lukhi-laksh <luxlukhi04@gmail.com>
2025-09-30 15:20:45 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-2221/Solution-1.py
@@ -0,0 +1,22 @@
+"""
+Find Triangular Sum of an Array
+
+"""
+class Solution(object):
+    def triangularSum(self, nums):
+        l=len(nums)
+        while(l!=1):
+            l=l-1
+            for i in range(l):
+                nums[i]=(nums[i]+nums[i+1])%10
+        return nums[0]
+
+sol = Solution()
+print(sol.triangularSum([1, 2, 3, 4, 5]))
+
+
+"""
+Time Complexity:  O(n²)
+Space Complexity: O(n²)
+
+"""
