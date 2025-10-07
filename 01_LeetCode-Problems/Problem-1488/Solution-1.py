ab1b68f610b94816540f3f34fbdf73370a1fecf0
lukhi-laksh <luxlukhi04@gmail.com>
2025-10-07 21:25:59 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-1488/Solution-1.py
@@ -0,0 +1,34 @@
+"""
+Avoid Flood in the City
+
+"""
+
+class Solution:
+    def avoidFlood(self, rains):
+        dry_lands = []    # Index of dry lands that are not used yet
+        last_flood = dict()    # index of previous flood
+        res = []      # result list to return
+        for i,r in enumerate(rains):
+            if r == 0:
+                dry_lands.append(i)
+                res.append(1)
+            else:
+                if r in last_flood:
+                    dried = False
+                    for j,dl in enumerate(dry_lands):
+                        if dl > last_flood[r]:
+                            res[dl] = r
+                            last_flood[r] = i
+                            del dry_lands[j]
+                            dried = True
+                            break
+                    if not dried:
+                        return []
+                else:
+                    last_flood[r] = i
+                res.append(-1)
+        return res
+    
+sol = Solution()
+print(sol.avoidFlood([1,2,3,4]))
+
