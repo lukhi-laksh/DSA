2db4b0173871c56ce7029b41c2727b609982e988
lukhi-laksh <luxlukhi04@gmail.com>
2025-10-18 22:41:15 +0530

    Problem Solved

--- 05_LeetCode-Database/Pandas/Query-584.py
+++ 05_LeetCode-Database/Pandas/Query-584.py
@@ -1,12 +1,11 @@
 import pandas as pd
-
+# Dataframe
 def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
-    # Filter the DataFrame based on the conditions
+
     filtered_customers = customer[
         (customer['referee_id'].isnull()) | (customer['referee_id'] != 2)
     ]
     
-    # Select only the 'name' column
     result = filtered_customers[['name']]
     
     return result    
