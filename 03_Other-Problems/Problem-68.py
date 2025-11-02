c5a9af4e45b14d1e2f76e6e127d5a4eb1a348524
lukhi-laksh <luxlukhi04@gmail.com>
2025-11-02 20:17:14 +0530

    Problem Solved

---
+++ 03_Other-Problems/Problem-68.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def getSneakyNumbers(self, nums):
+        freq = [0] * len(nums)
+        res = []
+        
+        for num in nums:
+            freq[num] += 1
+        
+        for i in range(len(freq)):
+            if freq[i] == 2:
+                res.append(i)
+        
+        return res
