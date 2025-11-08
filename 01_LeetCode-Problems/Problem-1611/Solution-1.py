09b476fb312cc62e2d2cc0a88ea2412cf400c56c
lukhi-laksh <luxlukhi04@gmail.com>
2025-11-08 22:30:36 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-1611/Solution-1.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def minimumOneBitOperations(self, n):
+        count = 0
+        while n>0:
+            count ^= n
+            n >>= 1
+        return count
+        
