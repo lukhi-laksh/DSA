c491206ba721ae1efc1c4362819ced9864b49d32
lukhi-laksh <luxlukhi04@gmail.com>
2025-11-20 22:09:08 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-757/Solution-1.py
@@ -0,0 +1,27 @@
+"""
+Set Intersection Size At Least Two
+
+"""
+
+class Solution(object):
+    def intersectionSizeTwo(self, intervals):
+
+        intervals.sort(key = lambda(s, e): (s, -e))
+        todo = [2] * len(intervals)
+        ans = 0
+        while intervals:
+            (s, e), t = intervals.pop(), todo.pop()
+            for p in range(s, s+t):
+                for i, (s0, e0) in enumerate(intervals):
+                    if todo[i] and p <= e0:
+                        todo[i] -= 1
+                ans += 1
+        return ans
+    
+
+"""
+Time Complexity:  O(n²)
+Space Complexity: O(n)
+
+"""
+
