cbd18081c15e0c08ea3a45059d8f91b2d6ef12b7
Lukhi Laksh <luxlukhi04@gmail.com>
2026-08-24 23:37:12 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-1872/Solution-1.py
@@ -0,0 +1,21 @@
+"""
+Stone Game VIII
+
+"""
+class Solution:
+    def stoneGameVIII(self, stones):
+        n = len(stones)
+        prefix = stones[:]
+        for i in range(1, n):
+            prefix[i] += prefix[i - 1]
+        best = prefix[n - 1]
+        for i in range(n - 2, 0, -1):
+            best = max(best, prefix[i] - best)
+
+        return best
+
+"""
+Time Complexity: O(n)
+Space Complexity: O(n)
+
+"""
