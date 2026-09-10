f5e72e017c1695f2f38f9fc0bb017767f229ac8b
Lukhi Laksh <luxlukhi04@gmail.com>
2026-09-10 23:12:47 +0530

    Prblem Solved

---
+++ 01_LeetCode-Problems/Problem-2265/Solution-1.py
@@ -0,0 +1,33 @@
+"""
+Count Nodes Equal to Average of Subtree
+
+"""
+class Pair(object):
+    def __init__(self, first, second):
+        self.first = first
+        self.second = second
+
+
+class Solution(object):
+    def averageOfSubtree(self, root):
+
+        self.count = 0
+
+        def postOrder(root):
+            if root is None:
+                return Pair(0, 0)
+
+            left = postOrder(root.left)
+            right = postOrder(root.right)
+
+            total_sum = left.first + right.first + root.val
+            node_count = left.second + right.second + 1
+
+            if total_sum // node_count == root.val:
+                self.count += 1
+
+            return Pair(total_sum, node_count)
+
+        postOrder(root)
+
+        return self.count
