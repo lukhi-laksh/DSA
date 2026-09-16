9256cc279441c40c56a7c547bef1bb7359262b81
Lukhi Laksh <luxlukhi04@gmail.com>
2026-09-16 23:37:40 +0530

    Problem Solved

---
+++ 01_LeetCode-Problems/Problem-1621/Solution-1.py
@@ -0,0 +1,23 @@
+"""
+Number of Sets of K non-Overlapping Line Segments
+
+"""
+class Solution(object):
+    def numberOfSets(self, n, k):
+        MOD = 10**9 + 7
+
+        N = n + k - 1
+        R = 2 * k
+
+        result = 1
+
+        for i in range(1, R + 1):
+            result = result * (N - R + i) // i
+
+        return result % MOD
+
+"""
+Time Complexity: O(n log n)
+Space Complexity: O(1)
+
+"""
